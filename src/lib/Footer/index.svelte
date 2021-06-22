7b76a7fb62fdc8df4e3ef3cc77d9fc39edf751d5
Tobias <twadseth@icloud.com>
2021-06-22 13:21:39 +0200

    Changed from heroku to vercel

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -33,13 +33,15 @@
 		</div>
 		<div class="flex-1 my-2">
 			<p>
-				Powered by <a href="https://heroku.com">
+				Powered by <a href="https://vercel.com">
 					<i class="icon">
-						<svg viewBox="0 0 128 128" class="w-3 h-3">
-							<path
-								fill="#6762A6"
-								d="M102.1 2H25.9C19.3 2 14 7.3 14 13.9v100.3c0 6.6 5.3 11.9 11.9 11.9h76.3c6.6 0 11.9-5.3 11.9-11.9V13.9C114 7.3 108.7 2 102.1 2zM37 108.7V80.5l14.1 14.1L37 108.7zm53 .3H76.9l.1-.2V59.4s3.1-11.8-39.7 4.8c-.1.2-.2-45.7-.2-45.7l13.9-.1v29.4s39-15.4 39 11.7V109zm-5.2-73H70c5.3-6 10.2-17 10.2-17h15.3s-2.6 7-10.7 17z"
-							/>
+						<svg
+							class="w-4 h-4"
+							viewBox="0 0 1155 1000"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M577.344 0L1154.69 1000H0L577.344 0Z" fill="black" />
 						</svg>
 					</i>
 				</a>
