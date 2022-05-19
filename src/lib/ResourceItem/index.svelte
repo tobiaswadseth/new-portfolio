3b25f010f235a24a12d7652fd98772bcf098a8a1
Tobias Wadseth <tobias.wadseth@infotiv.se>
2022-05-19 16:29:48 +0200

    fix: add language to resouce list

--- src/lib/ResourceItem/index.svelte
+++ src/lib/ResourceItem/index.svelte
@@ -2,11 +2,13 @@
 	export let title;
 	export let description;
     export let file;
+    export let lang;
 </script>
 
 <div class="my-4 card lg:card-side max-w-3xl shadow">
 	<div class="card-body">
-		<h2 class="card-title">{title}</h2>
+        <h2 class="card-title">{title}</h2>
+        <p><strong>N. B. This is written in {lang}</strong></p>
 		<p>{description}</p>
 		<div class="card-action mt-6">
 			<a class="btn btn-primary" href="/content/{file}" target="blank" rel="external">Read More</a>
